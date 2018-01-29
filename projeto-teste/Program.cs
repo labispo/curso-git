00e144215547b68191924dfc5dd187da4e8dab45
Leandro Almeida Bispo <leandroabispo@hotmail.com>
2018-01-29 19:02:07 -0200

    Acrescentada a pergunta sobre como voce esta hoje

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -10,6 +10,7 @@ namespace projeto_teste {
 
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
 
