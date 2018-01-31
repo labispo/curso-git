db26086e904c06ca91d043a1966431b3b9205733
Leandro Almeida Bispo <leandroabispo@hotmail.com>
2018-01-31 18:52:08 -0200

    Acrescentando tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,9 +8,11 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
 
+            Console.WriteLine("----------------------------------------------------");
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("----------------------------------------------------");
 
             Console.ReadLine();
 
